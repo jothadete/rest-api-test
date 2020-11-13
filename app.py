884bc983f73f3f14379bd08d111efc1864ee279a
jothadete <46389943+jothadete@users.noreply.github.com>
2020-11-13 12:48:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,47 +32,32 @@ class User(Resource):
 				return user, 200
 		return "User not found", 404
 
-
 	def post(self, name):
 		parser = reqparse.RequestParser()
 		parser.add_argument("age")
 		parser.add_argument("occupation")
 		args = parser.parse_args()
-
 		for user in users:
 			if(name == user["name"]):
 				return "User with name {} already exists".format(name), 400
-
-		user = {
-			"name": name,
-			"age": args["age"],
-			"occupation": args["occupation"]
+		user = {"name": name, "age": args["age"], "occupation": args["occupation"]
 		}
-
 		users.append(user)
 		return user, 201
-
 
 	def put(self, name):
 		parser = reqparse.RequestParser()
 		parser.add_argument("age")
 		parser.add_argument("occupation")
 		args = parser.parse_args()
-
 		for user in users:
 			if(name == user["name"]):
 				user["age"] = args["age"]
 				user["occupation"] = args["occupation"]
 				return user, 200
-
-		user = {
-			"name": name,
-			"age": args["age"],
-			"occupation": args["occupation"]
-		}
+		user = {"name": name, "age": args["age"], "occupation": args["occupation"]}
 		users.append(user)
 		return user, 201
-
 
 	def delete(self, name):
 		global users
@@ -80,6 +65,13 @@ class User(Resource):
 		return "{} is deleted.".format(name), 200
 
 api.add_resource(User, "/user/<string:name>")
+	
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+
 
 app.run(port=5000, debug=True)
 
